24588e5d369412b961b9946b1473329d4d3fcfec
xtito <7924023@qq.com>
2019-01-02 16:48:17 +0800

    优化计算工具类

--- oa-core/src/main/java/com/oa/core/utils/MathUtil.java
+++ oa-core/src/main/java/com/oa/core/utils/MathUtil.java
@@ -1,5 +1,8 @@
 package com.oa.core.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.math.BigDecimal;
 
 /**
@@ -9,6 +12,8 @@ import java.math.BigDecimal;
 public class MathUtil {
 
     private static final int DEF_DIV_SCALE = 10;// 默认除法运算精度
+    private static final int DEF_ROUND_SCALE = 2;// 默认四舍五入保留2位小数
+    private static final Logger logger = LoggerFactory.getLogger(MathUtil.class);
 
     private MathUtil() {
     }
@@ -66,6 +71,19 @@ public class MathUtil {
 
 
     /**
+     * 乘法算数运算
+     *
+     * @param v1 被乘数
+     * @param v2 乘数
+     * @param scale 表示需要精确到小数点以后几位
+     * @return 两数的积
+     */
+    public static BigDecimal mul(Object v1, Object v2, int scale) {
+        return mul(v1, v2).setScale(scale, BigDecimal.ROUND_HALF_UP);
+    }
+
+
+    /**
      * 除法算数运算
      * 提供（相对）精确的除法运算，当发生除不尽的情况时，精确到小数点后10位，以后的数字四舍五入
      *
@@ -92,9 +110,12 @@ public class MathUtil {
             throw new IllegalArgumentException("除法运算精度不能小于0");
         }
 
-        BigDecimal b1 = new BigDecimal(v1.toString());
-        BigDecimal b2 = new BigDecimal(v2.toString());
-        return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP);
+        if (v1 != null && v2 != null) {
+            BigDecimal b1 = new BigDecimal(v1.toString());
+            BigDecimal b2 = new BigDecimal(v2.toString());
+            return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP);
+        }
+        return new BigDecimal(0);
     }
 
 
@@ -110,8 +131,38 @@ public class MathUtil {
             throw new IllegalArgumentException("除法运算精度不能小于0");
         }
 
-        BigDecimal b1 = new BigDecimal(v.toString());
-        return b1.setScale(scale, BigDecimal.ROUND_HALF_UP);
+        if (v != null) {
+            BigDecimal b1 = new BigDecimal(v.toString());
+            return b1.setScale(scale, BigDecimal.ROUND_HALF_UP);
+        }
+        return new BigDecimal(0);
     }
 
+
+    /**
+     * 提供精确的小数位四舍五入处理，默认进行四五入并保留两位小数
+     *
+     * @param v     需要四舍五入的数字
+     * @return 四舍五入后的结果
+     */
+    public static BigDecimal round(Object v) {
+        return round(v, DEF_ROUND_SCALE);
+    }
+
+
+    /**
+     * 格式化数字，如果数字为空或为NULL 返回0
+     *
+     * @param v 要格式化的数字
+     * @return 返回格式化后数字，如果要格式化的对象为空，返回0
+     */
+    public static BigDecimal formatNumber(Object v) {
+        try {
+            return new BigDecimal(v.toString());
+        } catch (NumberFormatException e) {
+            String str = v != null ? v.toString() : "空对象";
+            logger.error(str + "转换数字失败！", e);
+        }
+        return new BigDecimal(0);
+    }
 }
